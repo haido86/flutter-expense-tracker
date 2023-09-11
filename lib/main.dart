c79bdb65fe54b5643bc7a561d5749372ac81c8d8
haido86 <thithuhai.do@gmail.com>
2023-09-11 11:33:21 +0300

    Add first class

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:expense_tracker/expenses.dart';
+import 'package:flutter/material.dart';
+
+// import 'package:expense_tracker/expenses.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+    ),
+  );
+}
