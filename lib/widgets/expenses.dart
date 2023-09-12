8519f5e3bbff44b4e50fb53f56645e94c8fe79e3
haido86 <thithuhai.do@gmail.com>
2023-09-12 13:31:39 +0300

    Save new expense

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -26,10 +26,18 @@ class _ExpensesState extends State<Expenses> {
         category: Category.leisure),
   ];
 
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registerExpenses.add(expense);
+    });
+  }
+
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
       context: context,
-      builder: ((ctx) => const NewExpense()),
+      builder: ((ctx) => NewExpense(
+            onAddExpense: _addExpense,
+          )),
     );
   }
 
